9f3acdfead47a39d1fbb9173258568e382cba249
Abhishek Vishwakarma <103137932+abhisharma8756@users.noreply.github.com>
2024-04-12 10:50:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const qrText = document.getElementById("qr-text");
+const sizes = document.getElementById("sizes");
+const generateBtn = document.getElementById("generateBtn");
+const downloadBtn = document.getElementById("downloadBtn");
+
+const qrContainer = document.querySelector(".qr-body");
+
+let size = sizes.value;
+generateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+ isEmpty()
+});
+
+sizes.addEventListener('change', (e)=>{
+    size = e.target.value;
+    isEmpty()
+    
+
+})
+
+
+downloadBtn.addEventListener("click",()=>{
+    let img = document.querySelector(".qr-body img")
+    if(img !== null){
+        let imgAtrr = img.getAttribute('src')
+        downloadBtn.setAttribute('href', imgAtrr)
+    }
+    else{
+        downloadBtn.setAttribute("href",`${document.querySelector('canvas').toDataURL()}`)
+    }
+})
+
+function isEmpty(){
+    // if(qrText.value.length > 0){
+
+    //     generateQRCode();
+    // }else{
+    //   alert("Enter the text or URL")
+    // }
+    qrText.value.length > 0 ? generateQRCode() : alert("Enter the text or URL")
+
+}
+function generateQRCode(){
+    qrContainer.innerHTML = "";
+    new QRCode(qrContainer, {
+        text:qrText.value,
+        width:sizes.value,
+        height:sizes.value,
+        colorLight: "#fff",
+        colorDark: "#000"
+    })
+}
